0b68c774f8614bfc7f77d5091f3e401a51b1c62f
onehitdragon <onehitdragon@gmail.com>
2022-07-31 06:14:17 +0700

    fix avatar listparticipant component item

--- src/components/ListParticipantGroup.js
+++ src/components/ListParticipantGroup.js
@@ -8,7 +8,7 @@ function ListParticipantGroup({creatorId, participants}){
         return (
             <div key={participant.id} className="list-participant__item">
                 <div className="avatar-area">
-                    <img className="avatar" src="https://cdn4.iconfinder.com/data/icons/game-of-thrones-4/64/game_of_thrones_game_thrones_series_character_avatar_ice_dragon-512.png" alt="error"/>
+                    <img className="avatar" src={participant.avatarUrl} alt="error"/>
                     <i className="fa-solid fa-circle icon-status--green"></i>
                 </div>
                 <div className="name-area">
